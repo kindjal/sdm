e550d2e87a38e3f55f06b550e1a54249e0da92b0
Matt Callaway <mcallawa@genome.wustl.edu>
2011-02-12 14:25:36 -0600

    Update DS

--- lib/System/DataSource/Disk.pm
+++ lib/System/DataSource/Disk.pm
@@ -1,14 +1,15 @@
 package System::DataSource::Disk;
-
 use strict;
 use warnings;
-
 use System;
 
 class System::DataSource::Disk {
     is => [ 'UR::DataSource::SQLite', 'UR::Singleton' ],
 };
 
-sub server { './Disk.sqlite3' }
+sub driver { 'SQLite' };
+
+sub server { 'Disk.sqlite3' };
+
 
 1;
