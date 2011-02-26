99b98dc2d7f5e064184b5ced46cd25ca945ef976
Matt Callaway <mcallawa@genome.wustl.edu>
2011-02-25 19:28:13 -0600

    s/Array/Filer/

--- lib/System/Disk/Filer/Command.pm
+++ lib/System/Disk/Filer/Command.pm
@@ -1,11 +1,11 @@
-package System::Disk::Array::Command;
+package System::Disk::Filer::Command;
 
 use strict;
 use warnings;
 
 use System;
 
-class System::Disk::Array::Command {
+class System::Disk::Filer::Command {
     is => 'Command',
     is_abstract => 1,
     doc => 'work with disk filers',
