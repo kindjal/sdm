2267f75802c677cb935b2005ca42153dc50149a2
Matt Callaway <mcallawa@genome.wustl.edu>
2011-02-26 09:12:34 -0600

    Latest after schema update

--- lib/System/Disk/Array.pm
+++ lib/System/Disk/Array.pm
@@ -5,18 +5,19 @@ use warnings;
 
 use System;
 class System::Disk::Array {
+    type_name => 'disk array',
     table_name => 'DISK_ARRAY',
     id_by => [
         array_id => { is => 'INTEGER' },
     ],
     has => [
+        System::Disk::Host => { is => 'System::Disk::Host', id_by => 'host_id', constraint_name => 'ARRAY_HOST_FK' },
         created            => { is => 'DATE', is_optional => 1 },
+        host_id            => { is => 'INTEGER' },
         last_modified      => { is => 'DATE', is_optional => 1 },
         model              => { is => 'VARCHAR(255)' },
         size               => { is => 'UNSIGNED INTEGER' },
         type               => { is => 'VARCHAR(255)' },
-        host_id            => { is => 'INTEGER' },
-        System::Disk::Host => { is => 'System::Disk::Host', id_by => 'host_id', constraint_name => 'ARRAY_HOST_FK' },
     ],
     schema_name => 'Disk',
     data_source => 'System::DataSource::Disk',
