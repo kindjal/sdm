9952958128fa17699303721578ce442df093b483
Matt Callaway <mcallawa@genome.wustl.edu>
2011-02-26 23:09:10 -0600

    Move optional

--- lib/System/Disk/Host.pm
+++ lib/System/Disk/Host.pm
@@ -7,18 +7,20 @@ use System;
 class System::Disk::Host {
     table_name => 'DISK_HOST',
     id_by => [
-        host_id => { is => 'INTEGER' },
+        host_id       => { is => 'INTEGER' },
     ],
     has => [
-        comments      => { is => 'VARCHAR(255)', is_optional => 1 },
-        created       => { is => 'DATE', is_optional => 1 },
         hostname      => { is => 'VARCHAR(255)' },
-        last_modified => { is => 'DATE', is_optional => 1 },
-        location      => { is => 'VARCHAR(255)', is_optional => 1 },
-        os            => { is => 'VARCHAR(255)', is_optional => 1 },
-        status        => { is => 'UNSIGNED INTEGER', is_optional => 1 },
         filer         => { is => 'System::Disk::Filer', id_by => 'filer_id', constraint_name => 'HOST_FILER_FK' },
         filer_id      => { is => 'INTEGER', implied_by => 'filer' },
+    ],
+    has_optional => [
+        comments      => { is => 'VARCHAR(255)' },
+        created       => { is => 'DATE' },
+        last_modified => { is => 'DATE' },
+        location      => { is => 'VARCHAR(255)' },
+        os            => { is => 'VARCHAR(255)' },
+        status        => { is => 'UNSIGNED INTEGER' },
     ],
     schema_name => 'Disk',
     data_source => 'System::DataSource::Disk',
