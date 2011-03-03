1c0f7955bdcb8c0ef9bbd325ce890e742fd8e649
Matt Callaway <mcallawa@genome.wustl.edu>
2011-03-03 11:42:19 -0600

    update

--- lib/System/adddata.sh
+++ lib/System/adddata.sh
@@ -3,17 +3,17 @@
 set -e
 set -x
 
-./system disk filer list --noheaders --filter name=localhost 2>/dev/null | grep -q localhost || \
-  ./system disk filer create --name localhost
+./system disk filer list --noheaders --filter name=nfs11 2>/dev/null | grep -q nfs11 || \
+  ./system disk filer create --name nfs11
 
-./system disk host list --noheaders --filter hostname=localhost 2>/dev/null | grep -q localhost || \
-  ./system disk host create --hostname localhost --filer localhost
+./system disk host list --noheaders --filter hostname=nfs11 2>/dev/null | grep -q nfs11 || \
+  ./system disk host create --hostname nfs11 --filer nfs11
 
-./system disk array list --noheaders --filter name=localdisk 2>/dev/null | grep -q localdisk || \
-  ./system disk array create --name localdisk --host localhost
+./system disk array list --noheaders --filter name=GCEVA3 2>/dev/null | grep -q GCEVA3 || \
+  ./system disk array create --name GCEVA3 --host nfs11
 
-./system disk volume list --noheaders --filter filer=localdisk 2>/dev/null | grep -q localhost || \
-  ./system disk volume create --filer=localhost --mount-path=/ --physical-path=/dev/sda1 --total-kb=19737268 --used-kb=7915612
+./system disk volume list --noheaders --filter filer=nfs11 2>/dev/null | grep -q sata821 || \
+  ./system disk volume create --filer=nfs11 --mount-path=/gscmnt/sata821 --physical-path=/vol/sata821 --total-kb=6438990688 --used-kb=5722964896
 
-./system disk group list --noheaders --filter name=mine 2>/dev/null | grep -q mine || \
-  ./system disk group create --name mine --permissions 755 --sticky 0 --unix-uid 12376 --unix-gid 10001
+./system disk group list --noheaders --filter name=PRODUCTION_SOLID 2>/dev/null | grep -q PRODUCTION_SOLID || \
+  ./system disk group create --name PRODUCTION_SOLID --permissions 755 --sticky 0 --unix-uid 12376 --unix-gid 10001
