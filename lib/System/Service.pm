9faea6e235ebffbf2ede6e08f14e63a778b50f6e
Matt Callaway <mcallawa@genome.wustl.edu>
2011-03-03 23:38:25 -0600

    Add service

---
+++ lib/System/Service.pm
@@ -0,0 +1,11 @@
+package System::Service;
+use strict;
+use warnings;
+use System;
+
+class System::Service {
+    is          => 'Command::Tree',
+    doc         => 'Work with services',
+};
+
+1;
