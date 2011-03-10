36d2f248e6211fabe7a5b794829419d52b7600de
Matt Callaway <mcallawa@genome.wustl.edu>
2011-03-10 09:45:23 -0600

    Build sql query to fetch and filter data.  Works except for UR bug APIPE-594.

--- lib/System/Disk/Volume/View/Table/Cgi.pm
+++ lib/System/Disk/Volume/View/Table/Cgi.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 
 use System;
+
 use JSON;
 use URI;
 use URI::QueryParam;
@@ -59,40 +60,149 @@ sub commify {
   return scalar reverse $text;
 }
 
+sub _fnColumnToField {
+  my $self = shift;
+  my $i = shift;
+
+  # Note: we could have used an array, but for dispatching purposes, this is
+  # more readable.
+  my %dispatcher = (
+    # column => 'rowname',
+    0 => 'mount_path',
+    1 => 'physical_path',
+    2 => 'total_kb',
+    3 => 'used_kb',
+    4 => 'capacity',
+    5 => 'filername',
+    6 => 'disk_group',
+    7 => 'last_modified',
+  );
+
+  die("No such row index defined: $i") unless exists $dispatcher{$i};
+
+  return $dispatcher{$i};
+} # /_fnColumnToField
+
+sub _generate_where_clause {
+  my $self = shift;
+  my $q = shift;
+
+  my @where;
+
+  if( defined $q->query_param('sSearch') ) {
+    my $search_string = $q->query_param('sSearch');
+    for( my $i = 0; $i < $q->query_param('iColumns'); $i++ ) {
+      # Iterate over each column and check if it is searchable.
+      # If so, add a constraint to the where clause restricting the given
+      # column. In the query, the column is identified by it's index, we
+      # need to translates the index to the column name.
+      my $searchable_ident = 'bSearchable_'.$i;
+      #if( $q->query_param($searchable_ident) and $q->query_param($searchable_ident) eq 'true' ) {
+      if( length $search_string > 0 and
+          $q->query_param($searchable_ident) and
+          $q->query_param($searchable_ident) eq 'true' ) {
+        my $column = $self->_fnColumnToField( $i );
+        push @where,"$column LIKE \"%%$search_string%%\"";
+      }
+    }
+  }
+
+  my $where;
+  $where .= " WHERE " . join(" OR ",@where) if (@where);
+  return $where;
+} # /_generate_where_clause
+
+sub _generate_order_clause {
+  my $self = shift;
+  my $q = shift;
+
+  my @order = ();
+  if( defined $q->query_param('iSortCol_0') ){
+    for( my $i = 0; $i < $q->query_param('iSortingCols'); $i++ ) {
+      # We only get the column index (starting from 0), so we have to
+      # translate the index into a column name.
+      my $column_name = $self->_fnColumnToField( $q->query_param('iSortCol_'.$i) );
+      my $direction = $q->query_param('sSortDir_'.$i);
+      push @order, "$column_name $direction";
+    }
+  }
+
+  my $order;
+  $order .= " ORDER BY " . join(',',@order) if (@order);
+  return $order;
+} # /_generate_order_clause
+
+sub _build_sql_select {
+    my ($self,$query) = @_;
+
+    # FIXME: UR defaults to an AND query and we want this to be an OR query.
+    # So we build our own SQL.
+    my $select = "SELECT mount_path, physical_path, total_kb, used_kb, ROUND((CAST(used_kb AS REAL)/total_kb * 100),2) as capacity, filername, disk_group, last_modified FROM disk_volume";
+
+    # -- Filtering: applies DataTables search box
+    my $where = $self->_generate_where_clause($query);
+    if ($where) {
+        $select .= $where;
+    }
+
+    # -- Ordering: makes DataTables column sorting work
+    my $order = $self->_generate_order_clause($query);
+    if ($order) {
+        $select .= $order;
+    }
+
+    # -- Paging
+    my $paging;
+    my $limit = $query->query_param('iDisplayLength') || 10;
+    if ($limit) {
+        $paging .= " LIMIT $limit";
+    }
+    my $offset = 0;
+    if( $query->query_param('iDisplayStart') ) {
+        $offset = $query->query_param('iDisplayStart');
+    }
+    if ($offset) {
+        $paging .= " OFFSET $offset";
+    }
+    $select .= $paging;
+    return $select;
+}
+
 sub run {
 
     my ($self,$args) = @_;
 
     my $json = new JSON;
-
-    my $query = URI->new($args->{REQUEST_URI});
-    my $param = $query->query_param;
-
-    # FIXME: Build params based on query string search terms
     my @aaData;
-    #my $params;
-    foreach my $v (System::Disk::Volume->get( )) {
-        my $mount_path = $v->{mount_path};
-        my $physical_path = $v->{physical_path};
-        my $total_kb = $v->{total_kb} ? $v->{total_kb} : 0;
-        my $used_kb = $v->{used_kb} ? $v->{used_kb} : 0;
 
-        my $percentage = 0;
-        if ( $used_kb > 0 and $total_kb > 0 ) {
-            $percentage = sprintf "%d %%", $used_kb / $total_kb * 100;
-        }
-        $total_kb = $self->commify($total_kb) . " (" . $self->short($total_kb) . ")";
-        $used_kb = $self->commify($used_kb) . " (" . $self->short($used_kb) . ")";
+    my $query = URI->new( $args->{REQUEST_URI} );
 
-        my $disk_group = $v->{disk_group} ? $v->{disk_group} : 'unknown';
-        my $last_modified = $v->{last_modified} ? $v->{last_modified} : 'unknown';
-
-        push @aaData, [$mount_path,$physical_path,$total_kb,$used_kb,$percentage,$disk_group,$last_modified];
+    # UR Missing feature: Can't to OR clauses in get()
+    # So try building SQL for get()
+    # UR BUG: duplicate data somehow?
+    my @vols = System::Disk::Volume->get( sql => $self->_build_sql_select( $query ) );
+    # Next try:
+    # 1 get() for each searchable column
+    # build the hash with key of object id in result set
+    foreach my $v (@vols) {
+        push @aaData, [
+            $v->{mount_path},
+            $v->{physical_path},
+            $self->commify($v->{total_kb}) . " (" . $self->short($v->{total_kb}) . ")",
+            $self->commify($v->{used_kb}) . " (" . $self->short($v->{used_kb}) . ")",
+            sprintf("%d %%", $v->{capacity} ? $v->{capacity} : 0 ),
+            $v->{filername},
+            $v->{disk_group} ? $v->{disk_group} : 'unknown',
+            $v->{last_modified} ? $v->{last_modified} : 'unknown'
+        ];
     }
 
     my $sEcho = defined $query->query_param('sEcho') ? $query->query_param('sEcho') : 1;
-    my $iTotal = $self->_get_total_record_count();
-    my $iFilteredTotal = $self->_get_filtered_record_count();
+    # FIXME: find total?
+    my @results = System::Disk::Volume->get();
+    my $iTotal = scalar @results;
+    warn "DEBUG total $iTotal\n";
+    my $iFilteredTotal = scalar @vols;
     my $sOutput = {
         sEcho => $sEcho,
         iTotalRecords => int($iTotal),
