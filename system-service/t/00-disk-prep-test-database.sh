4136e4a97454d98255a6abf86f8b54b6a8247001
Matt Callaway <mcallawa@genome.wustl.edu>
2011-03-23 15:39:23 -0500

    Add test and package

---
+++ system-service/t/00-disk-prep-test-database.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+set -e
+
+die () {
+    echo "Error: $@"
+    exit 1
+}
+
+trap die ERR
+
+TOP=`pwd`
+BASE=$TOP/lib/System/
+PATH="/bin:/usr/bin:/sbin:/usr/sbin"
+PERL="/usr/bin/perl -I $TOP/lib -I ../system/lib ../system-*/lib"
+SYSTEM="../system/bin/system"
+
+[ -d $BASE ] || \
+    die "$BASE not found"
+
+[ -x $SYSTEM ] || \
+    die "$SYSTEM not found"
+
+# convenience script to "start from scratch"
+echo flush sqlite3 DB
+rm -f $BASE/DataSource/Disk.sqlite3
+rm -f $BASE/DataSource/Disk.sqlite3-dump
+rm -f $BASE/DataSource/Disk.sqlite3n
+rm -f $BASE/DataSource/Disk.sqlite3n-dump
+
+echo flush and remake Meta
+cd ..
+rm -f $BASE/DataSource/Meta.sqlite3
+rm -f $BASE/DataSource/Meta.sqlite3n
+rm -f $BASE/DataSource/Meta.sqlite3-dump
+rm -f $BASE/DataSource/Meta.sqlite3n-dump
+cd -
+
+echo make new sqlite3 DB
+sqlite3 $BASE/DataSource/Disk.sqlite3n < $BASE/DataSource/Disk.sqlite3n-schema
+sqlite3 $BASE/DataSource/Disk.sqlite3n .dump > $BASE/DataSource/Disk.sqlite3n-dump
+
