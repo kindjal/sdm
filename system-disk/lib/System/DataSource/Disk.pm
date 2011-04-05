61123e4a9ca13b21be5e6dfa08044931fcf8c441
Matt Callaway <mcallawa@genome.wustl.edu>
2011-04-05 16:20:56 -0500

    Set for psql

--- system-disk/lib/System/DataSource/Disk.pm
+++ system-disk/lib/System/DataSource/Disk.pm
@@ -4,18 +4,26 @@ use warnings;
 use System;
 
 class System::DataSource::Disk {
-    is => [ 'UR::DataSource::SQLite', 'UR::Singleton' ],
+    #is => [ 'UR::DataSource::SQLite', 'UR::Singleton' ],
+    is => [ 'UR::DataSource::Pg' ],
 };
 
-sub driver { 'SQLite' };
-sub dump_on_commit { 1 };
+#sub driver { 'SQLite' };
+#sub dump_on_commit { 1 };
 
-sub create_dbh {
-    my $self = shift->_singleton_object();
-    $self->_init_database;
-    my $dbh = $self->SUPER::create_dbh(@_);
-    $dbh->do("PRAGMA foreign_keys = ON");
-    return $dbh;
-}
+sub driver { "Pg"; }
+sub server { "dbname=system"; }
+
+sub owner  { 'public'; }
+sub login  { 'system'; }
+sub auth   { 'test'; }
+
+#sub create_dbh {
+#    my $self = shift->_singleton_object();
+#    $self->_init_database;
+#    my $dbh = $self->SUPER::create_dbh(@_);
+#    $dbh->do("PRAGMA foreign_keys = ON");
+#    return $dbh;
+#}
 
 1;
