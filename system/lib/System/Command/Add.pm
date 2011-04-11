6f76b4c73d21a2cb7f7648857fea9078ab583a69
Matt Callaway <mcallawa@genome.wustl.edu>
2011-04-11 15:59:18 -0500

    add a check for existing object

--- system/lib/System/Command/Add.pm
+++ system/lib/System/Command/Add.pm
@@ -49,10 +49,12 @@ sub execute {
     }
     #$self->status_message(Dumper(\%attrs));
 
+    # Find out if we already have an Object whose id matches that which we are about to create
     my $target_class = $self->_target_class;
-    my $res = $target_class->get(%attrs);
+    my $id = pop @{ [ grep { $_->is_id } $target_class->__meta__->property_metas ] };
+    my $res = $target_class->get( $id->attribute_name => $attrs{$id->attribute_name} );
     if ($res) {
-        $self->error_message('Object already exists:\n' . Dumper(\%attrs));
+        $self->error_message("'$target_class' object already exists with this id: " . $attrs{$id->attribute_name} );
         return;
     }
     my $obj = $target_class->create(%attrs);
