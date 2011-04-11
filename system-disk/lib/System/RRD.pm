bd88f23461d2d9ea606aa345a1007a6fa22ce6ba
Matt Callaway <mcallawa@genome.wustl.edu>
2011-04-11 11:00:11 -0500

    Add rrd work in progress

---
+++ system-disk/lib/System/RRD.pm
@@ -0,0 +1,12 @@
+
+package System::RRD;
+
+use warnings;
+use strict;
+
+use System;
+
+class System::RRD {
+    is => [ 'UR::Namespace' ],
+    type_name => 'rrd',
+};
---
+++ system-disk/lib/System/RRD/DiskGroup.pm
@@ -0,0 +1,41 @@
+
+package System::RRD::DiskGroup;
+
+use System::RRD;
+use Smart::Comments -ENV;
+
+class System::RRD::DiskGroup {
+    id_by => [
+        filename => { is => 'Text' },
+    ],
+    has => [
+        name              => { is => 'Text' },
+        type              => { is => 'Text' },
+        minimal_heartbeat => { is => 'Number' },
+        min               => { is => 'Number' },
+        max               => { is => 'Number' },
+        last_ds           => { is => 'Number' },
+        value             => { is => 'Number' },
+        unknown_sec       => { is => 'Number' },
+    ],
+    data_source => UR::DataSource::Default->create(),
+};
+
+sub __load__ {
+    my ($self, $bx, $headers) = @_;
+    my @header = ( 'name', 'type', 'minimal_heartbeat', 'min', 'max', 'last_ds', 'value', 'unknown_sec' );
+    my @rows = ();
+    push @rows,  [ 'filename', 'id', 'name', 'type', 'minimal_heartbeat', 'min', 'max', 'last_ds', 'value', 'unknown_sec' ];
+    unshift @header, 'filename';
+    unshift @header, 'id';
+
+    my $fn = $bx->value_for('filename');
+    ### fn: $fn
+    my $rrd = RRDTool::OO->new( file => $fn );
+    print $rrd->info();
+
+    return \@header, sub { shift @rows; }
+
+}
+
+1;
