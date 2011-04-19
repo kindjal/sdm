95909606cc0416406c51e67fa709a7dcc36d7218
Matt Callaway <mcallawa@genome.wustl.edu>
2011-04-19 10:00:20 -0500

    Re-add two methods removed from UR V2.pm

--- system/lib/System/Command/Base.pm
+++ system/lib/System/Command/Base.pm
@@ -10,4 +10,48 @@ class System::Command::Base {
     is_abstract => 1,
 };
 
+sub _ask_user_question {
+    my $self = shift;
+    my $question = shift;    my $timeout = shift;
+    my $valid_values = shift || "yes|no";
+    my $default_value = shift || undef;
+    my $pretty_valid_values = shift || $valid_values;
+    $valid_values = lc($valid_values);
+    my $input;
+    $timeout = 60 unless(defined($timeout));
+
+    local $SIG{ALRM} = sub { print STDERR "Exiting, failed to reply to question '$question' within '$timeout' seconds.\n"; exit; };
+    print STDERR "\n$question\n";
+    print STDERR "Reply with $pretty_valid_values: ";
+
+    unless ($self->_can_interact_with_user) {
+        die $self->error_message("Attempting to ask user question but cannot interact with user!");
+    }
+    alarm($timeout) if ($timeout);
+    chomp($input = <STDIN>);    alarm(0) if ($timeout);
+
+    print STDERR "\n";
+
+    if(lc($input) =~ /^$valid_values$/) {
+        return lc($input);
+    }
+    elsif ($default_value) {
+        return $default_value;
+    }
+    else {
+        $self->error_message("'$input' is an invalid answer to question '$question'\n\n");
+        return;
+    }
+}
+
+sub _can_interact_with_user {
+    my $self = shift;
+    if ( -t STDERR ) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 1;
