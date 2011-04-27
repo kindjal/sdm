aef2cfc3bc3413a3643d858625563e3e6faef201
Matt Callaway <mcallawa@genome.wustl.edu>
2011-04-27 15:04:12 -0500

    Use new base class and clean up

--- system-disk/lib/System/Disk/Filer/View/Status/Cgi.pm
+++ system-disk/lib/System/Disk/Filer/View/Status/Cgi.pm
@@ -5,20 +5,20 @@ use strict;
 use warnings;
 
 use System;
-
 use JSON;
 use URI;
 use URI::QueryParam;
 
-sub new {
-    my ($class,$args) = @_;
-    my $self = {};
-    bless $self, $class;
-    return $self;
-}
+class System::Disk::Filer::View::Status::Cgi {
+    is => 'System::Disk::View::Cgi'
+};
 
+=head2 _fnColumnToField
+This maps a column in DataTables to a UR::Object attribute.
+=cut
 sub _fnColumnToField {
     my $self = shift;
+    $self->{logger}->debug("_fnColumnToField: map DataTable column to UR::Object attribute");
     my $i = shift;
 
     # Note: we could have used an array, but for dispatching purposes, this is
@@ -38,120 +38,57 @@ sub _fnColumnToField {
     return $dispatcher{$i};
 }
 
-sub _build_order_param {
-    # FIXME: UR does not have ASC and DESC, it's always ASC
-    my ($self,$q) = @_;
-    my @order;
-    if( defined $q->query_param('iSortCol_0') ){
-        for( my $i = 0; $i < $q->query_param('iSortingCols'); $i++ ) {
-            # We only get the column index (starting from 0), so we have to
-            # translate the index into a column name.
-            my $column_name = $self->_fnColumnToField( $q->query_param('iSortCol_'.$i) );
-            my $direction = $q->query_param('sSortDir_'.$i);
-            if ($direction eq 'desc') { 
-                $column_name = "-$column_name";
-            } elsif ($direction eq 'asc') {
-                $column_name = "+$column_name";
-            }
-            push @order, $column_name;
-        }
-    }
-    return @order;
-}
-
-sub _build_where_param {
-    my ($self,$q) = @_;
-    my @where;
-    if( defined $q->query_param('sSearch') ) {
-        my $search_string = $q->query_param('sSearch');
-        for( my $i = 0; $i < $q->query_param('iColumns'); $i++ ) {
-            # Iterate over each column and check if it is searchable.
-            # If so, add a constraint to the where clause restricting the given
-            # column. In the query, the column is identified by it's index, we
-            # need to translates the index to the column name.
-            my $searchable_ident = 'bSearchable_'.$i;
-            if( length $search_string > 0 and
-                    $q->query_param($searchable_ident) and
-                    $q->query_param($searchable_ident) eq 'true' ) {
-                my $column = $self->_fnColumnToField( $i );
-                push @where, { "$column like" => "%%$search_string%%" };
-            }
-        }
-    }
-    return @where;
-}
-
+=head2 _build_result_set
+Get the set of Filers represented by a DataTables query.
+=cut
 sub _build_result_set {
-    # This requires UR beyond 94fbaa5086fc252078d2c25f368468bc76605e14
-    # to support the -or clause.
-    # FIXME: we still want LIMIT and OFFSET.
     my ($self,$q) = @_;
+    $self->{logger}->debug("_build_result_set: fetch UR::Objects and return a UR::Object::Set");
+
     my $param = {};
     my @where = $self->_build_where_param($q);
-    my @order = $self->_build_order_param($q);
     if (scalar @where) {
-        $param->{ -or } = [ \@where ];
+        $param->{ -or } = \@where;
     }
-    # FIXME: UR order can't do DESC
-    if (scalar @order) {
-        $param->{ -order } = \@order;
-    }
+
     my @result = System::Disk::Filer->get( $param );
-    # Implement limit and offset here to make up for lack of feature in get();
-    sub max ($$) { int($_[ $_[0] < $_[1] ]) };
-    sub min ($$) { int($_[ $_[0] > $_[1] ]) };
-    my $limit  = $q->query_param('iDisplayLength') || 10;
-    my $offset = $q->query_param('iDisplayStart') || 0;
-    my $ceiling = min($limit - 1,$#result);
-    @result = @result[$offset..$ceiling];
     return @result;
 }
 
-sub run {
-
-    my ($self,$args) = @_;
-
-    my $json = new JSON;
-    my @aaData;
-
-    my $query = URI->new( $args->{REQUEST_URI} );
-
-    my @filers = $self->_build_result_set( $query );
-    foreach my $f ( @filers ) {
+=head2 _build_aadata
+Order and sort our UR::Object::Set as well as applying some modifiers and transformations.
+=cut
+sub _build_aadata {
+    my $self = shift;
+    $self->{logger}->debug("_build_aadata: convert a UR::Object::Set");
+    my $query = shift;
+    my @results = @_;
+    return unless (@results and $results[0]->isa( 'System::Disk::Filer' ) );
+    my @data;
+    foreach my $item ( @results ) {
         my $hostname = 'unknown';
-        my @hosts = $f->hostname;
-        if ($#hosts) {
+        my @hosts = $item->hostname;
+        if (@hosts) {
             $hostname = join(",",@hosts);
         }
         my $arrayname = 'unknown';
-        my @arrays = $f->arrayname;
-        if ($#arrays) {
+        my @arrays = $item->arrayname;
+        if (@arrays) {
             $arrayname = join(",",@arrays);
         }
 
-        push @aaData, [
-            $f->{name},
-            $f->{status},
+        push @data, [
+            $item->{name},
+            $item->{status},
             $hostname,
             $arrayname,
-            $f->{created} ?  $f->{created} : "0000-00-00 00:00:00",
-            $f->{last_modified} ? $f->{last_modified} : "0000-00-00 00:00:00",
+            $item->{created} ? $item->{created} : "0000-00-00 00:00:00",
+            $item->{last_modified} ? $item->{last_modified} : "0000-00-00 00:00:00",
         ];
     }
 
-    my $sEcho = defined $query->query_param('sEcho') ? $query->query_param('sEcho') : 1;
-    my @results = System::Disk::Filer->get();
-    my $iTotal = scalar @results;
-    my $iFilteredTotal = scalar @filers;
-    my $sOutput = {
-        sEcho => $sEcho,
-        iTotalRecords => int($iTotal),
-        iTotalDisplayRecords => int($iFilteredTotal),
-        aaData => \@aaData,
-    };
-
-    my $result = $json->encode($sOutput);
-    return $result
+    my @sorted_data = $self->_sorter($query,@data);
+    return @sorted_data;
 }
 
 1;
