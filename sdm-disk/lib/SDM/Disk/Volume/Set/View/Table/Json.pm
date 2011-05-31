e87b1375ff295b2c353684f3471abe4df3c3f05b
Matt Callaway <mcallawa@genome.wustl.edu>
2011-05-31 10:38:07 -0500

    Same aspects as XML

--- sdm-disk/lib/SDM/Disk/Volume/Set/View/Table/Json.pm
+++ sdm-disk/lib/SDM/Disk/Volume/Set/View/Table/Json.pm
@@ -8,28 +8,28 @@ use SDM;
 
 class SDM::Disk::Volume::Set::View::Table::Json{
     is => 'UR::Object::Set::View::Default::Json',
-    #has_constant => [
-    #    default_aspects => {
-    #        is => 'ARRAY',
-    #        value => [
-    #            rule_display => {
-    #                name => 'members',
-    #                perspective => 'default',
-    #                toolkit => 'json',
-    #                subject_class_name => 'SDM::Disk::Volume',
-    #                aspects => [
-    #                    'mount_path',
-    #                    'total_kb',
-    #                    'used_kb',
-    #                    'capacity',
-    #                    'disk_group',
-    #                    'filername',
-    #                    'last_modified',
-    #                ]
-    #            }
-    #        ]
-    #    }
-    #]
+    has_constant => [
+        default_aspects => {
+            is => 'ARRAY',
+            value => [
+                rule_display => {
+                    name => 'members',
+                    perspective => 'default',
+                    toolkit => 'json',
+                    subject_class_name => 'SDM::Disk::Volume',
+                    aspects => [
+                        'mount_path',
+                        'total_kb',
+                        'used_kb',
+                        'capacity',
+                        'disk_group',
+                        'filername',
+                        'last_modified',
+                    ]
+                }
+            ]
+        }
+    ]
 };
 
 1;
