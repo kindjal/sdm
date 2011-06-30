ee8415d0cf7d3d3a46e7ffbec8a3026605c47d14
Matt Callaway <mcallawa@genome.wustl.edu>
2011-06-30 14:49:58 -0500

    Add update method.  Add files and filename relationship.

--- sdm-service/lib/SDM/Service/Lsof/Process.pm
+++ sdm-service/lib/SDM/Service/Lsof/Process.pm
@@ -3,6 +3,7 @@ package SDM::Service::Lsof::Process;
 
 use strict;
 use warnings;
+use feature "switch";
 
 use SDM;
 
@@ -23,7 +24,8 @@ class SDM::Service::Lsof::Process {
         command       => { is => 'Text', default => '' },
     ],
     has_many_optional => [
-        files         => { is => 'SDM::Process::Lsof::File', reverse_as => 'process' },
+        files         => { is => 'SDM::Service::Lsof::File', reverse_as => 'process' },
+        filename      => { is => 'Text', via => 'files' },
     ],
     has_optional => [
         created       => { is => 'Date' },
@@ -31,6 +33,47 @@ class SDM::Service::Lsof::Process {
     ],
 };
 
+=head2 create
+Update a Process record... this is primarily to handle the filenames part.
+=cut
+sub update {
+    my $self = shift;
+    my $record = shift;
+    foreach my $attr (keys %$record) {
+        given ($attr) {
+            when (/^name$/) {
+                # Ensure all new files are stored
+                foreach my $file (@{ $record->{'name'} }) {
+                    my $result = SDM::Service::Lsof::File->get_or_create( hostname => $self->hostname, pid => $self->pid, filename => $file );
+                    unless ($result) {
+                        $self->error_message("failed to create file object $file for " . $self->pid);
+                    }
+                }
+                # Remove stored files that aren't currently open
+                foreach my $file ( SDM::Service::Lsof::File->get( hostname => $self->hostname, pid => $self->pid) ) {
+                    my $name = $file->filename;
+                    unless ( not grep { /^$name$/ } @{ $record->{'name'} } ) {
+                        $file->delete;
+                    }
+                }
+            }
+            when (/^timedelta$/) {
+                $self->timedelta( $record->{timedelta} );
+            }
+            when (/^last_modified$/) {
+                $self->last_modified( Date::Format::time2str(q|%Y-%m-%d %H:%M:%S|,time()) );
+            }
+            default {
+                my $p = $self->__meta__->property($attr);
+                $self->$attr( $record->{$attr} ) if (! $p->is_id and $p->is_mutable);
+            }
+        }
+    }
+}
+
+=head2 create
+Create a new Lsof::Process record.
+=cut
 sub create {
     my $self = shift;
     my ($params) = @_;
