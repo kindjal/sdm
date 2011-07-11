945072fb439c781d536fc972b4294386f97b91a7
Matt Callaway <mcallawa@genome.wustl.edu>
2011-07-11 13:56:24 -0500

    Don't sync sqlite3n

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 set -ex
 mkdir -p deploy/bin
 for dir in sdm sdm-*; do
-    rsync -av $dir/lib/ deploy/lib/
-    rsync -av $dir/t/ deploy/t/
+    [ -d $dir/lib ] && \
+      rsync -av --exclude '*.sqlite3n' $dir/lib/ deploy/lib/
+    [ -d $dir/t ] && \
+      rsync -av $dir/t/ deploy/t/
 done
 cp sdm/bin/sdm deploy/bin/sdm
