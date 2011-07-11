2c900509d8c180d7d6b0e5b05ae7c6fdd7551b2f
Matt Callaway <mcallawa@genome.wustl.edu>
2011-07-11 16:53:18 -0500

    add host table view

---
+++ sdm-disk/lib/SDM/View/Resource/Html/js/app/hosttable.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function() {
+  TableToolsInit.sSwfPath = "/res/js/pkg/TableTools/media/swf/ZeroClipboard.swf";
+
+  /* data table */
+  var aTable = $('#hosttable').dataTable( {
+    "sDom": 'T<"clear">lfrtip',
+    "bProcessing": true,
+    "iDisplayLength": 25,
+    "sPaginationType": "full_numbers",
+    "bAutoWidth": false,
+    "aaSorting": [ [1,'desc'] ],
+    "fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+        $('td',nRow).each( function (iPosition) {
+            if (this.title) {
+                $('td:eq(' + iPosition + ')',nRow).html( this.title );
+            }
+        } );
+        return nRow;
+    },
+    "aoColumns": [
+      { "sWidth": "10%" },
+      { "sWidth": "15%"  },
+      { "sWidth": "10%" },
+      { "sWidth": "10%" },
+      { "sWidth": "15%"  },
+      { "sWidth": "3%" },
+      { "sWidth": "15%" },
+      { "sWidth": "12%" },
+      { "sWidth": "12%" }
+    ],
+  } );
+  /* end data table */
+
+} ); /* end document ready function */
+
