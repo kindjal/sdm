0d23b61c6cc5c6add635d930e8a6ee8b6da4c793
Matt Callaway <mcallawa@genome.wustl.edu>
2011-07-20 13:06:33 -0500

    add some caluclated relations

--- sdm-service/lib/SDM/Service/Lsof/File.pm
+++ sdm-service/lib/SDM/Service/Lsof/File.pm
@@ -16,6 +16,38 @@ class SDM::Service::Lsof::File {
     ],
     has => [
         filename     => { is => 'Text' },
+        mountpoint   => {
+            is_calculated => 1,
+            calculate_from => 'filename',
+            calculate => q| $filename =~ m/\S+\s\((\S+)\)/; return $1; |,
+        },
+        server => {
+            is_calculated => 1,
+            calculate_from => 'filename',
+            calculate => q| $filename =~ m/\S+\s\((\S+):(\S+)\)/; return $1; |,
+        },
+        filername => {
+            is_calculated => 1,
+            calculate_from => 'server',
+            calculate => q| my $h = shift @{ [ gethostbyname($server) ] }; my @g = split(/\./,$h,2); return $g[0]; |,
+        },
+        physical_path => {
+            is_calculated => 1,
+            calculate_from => 'filename',
+            calculate => q| $filename =~ m/\S+\s\((\S+):(\S+)\)/; return $2; |,
+        },
+        export => {
+            is => 'SDM::Disk::Export',
+            is_calculated => 1,
+            calculate_from => [ 'filername', 'physical_path' ],
+            calculate => q| return SDM::Disk::Export->get( filername => $filername, physical_path => $physical_path ); |,
+        },
+        volume => {
+            is => 'SDM::Disk::Volume',
+            is_calculated => 1,
+            calculate_from => [ 'filername', 'physical_path' ],
+            calculate => q| return SDM::Disk::Volume->get( filername => $filername, physical_path => $physical_path ); |,
+        },
         hostname     => { is => 'Text' },
         pid          => { is => 'Integer' },
         process      => { is => 'SDM::Service::Lsof::Process', id_by => ['hostname','pid'] }
