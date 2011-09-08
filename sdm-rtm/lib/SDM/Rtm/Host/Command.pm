242c3067e81857d7ff3c89a2171ef09433523c4c
Matt Callaway <mcallawa@genome.wustl.edu>
2011-09-08 13:47:09 -0500

    Fix Host definition

--- sdm-rtm/lib/SDM/Rtm/Host/Command.pm
+++ sdm-rtm/lib/SDM/Rtm/Host/Command.pm
@@ -8,15 +8,16 @@ use SDM;
 class SDM::Rtm::Host::Command {
     is          => 'Command::Tree',
     doc         => 'work with RTM hosts',
-    # leave as abstract until we know how to use this class
-    is_abstract => 1,
 };
 
 use SDM::Command::Crud;
 SDM::Command::Crud->init_sub_commands(
     target_class => 'SDM::Rtm::Host',
     target_name => 'host',
-    list => { show => 'hostname' }
+    list => { show => 'host,status,hCtrlMsg,maxJobs,numJobs,numRun' },
+    delete => { do_not_init => 1, },
+    update => { do_not_init => 1, },
+    add    => { do_not_init => 1, },
 );
 
 1;
