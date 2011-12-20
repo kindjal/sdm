546b06e611f99df69e0b1a703903efb1d845252b
Matt Callaway <mcallawa@genome.wustl.edu>
2011-12-20 13:52:19 -0600

    Define aspects here for use with javascript web view.

--- sdm-asset/lib/Sdm/Asset/Software.pm
+++ sdm-asset/lib/Sdm/Asset/Software.pm
@@ -23,6 +23,18 @@ class Sdm::Asset::Software {
         comments      => { is => 'Text' },
         created       => { is => 'Date' },
         last_modified => { is => 'Date' },
+    ],
+    has_constant => [
+        default_aspects => {
+            column_name => '',
+            doc => 'This is used by the web UI to draw a jquery-datatables view of a set of objects. Here we specify which attributes are visible and their order, and which attributes should be editable.',
+            is_classwide => 1,
+            is => 'HASH',
+            value => {
+                'visible'  => ['manufacturer','product','license','description','seats','comments','created','last_modified'],
+                'editable' => ['manufacturer','product','license','seats','description','comments'],
+            }
+        }
     ]
 };
 
